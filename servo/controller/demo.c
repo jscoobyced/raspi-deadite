f5c0ace9691b89db79a2363635023c10f31b2971
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2019-12-16 22:32:52 +0700

    Finally got it: - call and set position first - getPosition only on 1 servo (the one that will complete last)
    Also improve demo to use speed parameter;

--- servo/controller/demo.c
+++ servo/controller/demo.c
@@ -1,10 +1,8 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "uart.h"
 #include "controller.h"
-
-int speed = 100;
-
 
 void wait(unsigned char channel, unsigned int position)
 {
@@ -14,27 +12,34 @@ void wait(unsigned char channel, unsigned int position)
     }
 }
 
-void doMove(int angle)
+void doMove(int angle1, int angle2, int speed)
 {
-    setAngle(1, angle, speed);
-    wait(2, setAngle(2, angle, speed));
+    setAngle(1, angle1, speed);
+    wait(2, setAngle(2, angle2, speed));
 }
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
+    if (argc != 3)
     {
-        printf("You must pass the serial device as argument.\n");
+        printf("You must pass:\n- the serial device as first argument\n- the speed (0-100) as second argument.\n");
     }
 
     if (startUart(argv[1]) != 0)
     {
         return 0;
     }
+    char *endptr;
+    int speed = strtol(argv[1], &endptr, 10);
+    if(speed == 0) {
+        speed = 100;
+    }
     enableDisableChannel(0, 1);
-    doMove(0);
-    doMove(-90);
-    doMove(90);
+    doMove(0, 0, speed);
+    doMove(-90, 90, speed);
+    doMove(90, -90, speed);
+    doMove(-45, 0, speed);
+    doMove(45, 90, speed);
     enableDisableChannel(0, 0);
     closeUart();
     return 0;
