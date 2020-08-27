5aecb82b718d10513d14b7ecf46b2519eb07c360
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2020-08-27 22:53:46 +0700

    fix mounted drive

--- raspi/start-raspi.sh
+++ raspi/start-raspi.sh
@@ -15,7 +15,9 @@ mount -t sysfs none chroot-dir/sys
 
 chroot chroot-dir
 
+sleep 1
 umount chroot-dir/dev/pts
 umount chroot-dir/dev
 umount chroot-dir/proc
 umount chroot-dir/sys
+umount chroot-dir/usr/local/src/servo
